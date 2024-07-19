1965d68b917d356b568d889a9508d3bb48a4dfc3
vakulrai <vakulrai42@gmail.com>
2024-07-19 17:42:43 +0530

    Test runner.

---
+++ docroot/modules/custom/training_caching/src/Plugin/Block/TrainingCaching.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Drupal\training_caching\Plugin\Block;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Entity\Entity\EntityViewDisplay;
+use Drupal\Core\Entity\EntityRepositoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\training_caching\Services\TrainingDrupalApiManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a Story summary block.
+ *
+ * @Block(
+ *  id = "training_caching_ex1",
+ *  admin_label = @Translation("Cache tag and context"),
+ *  context_definitions = {
+ *    "node" = @ContextDefinition("entity:node", label = @Translation("Node"))
+ *  }
+ * )
+ */
+class TrainingCaching extends BlockBase implements ContainerFactoryPluginInterface {
+
+ /**
+   * Drupal\Core\Entity\EntityTypeManagerInterface definition.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Drupal\training_caching\Services\TrainingDrupalApiManager definition.
+   *
+   * @var \Drupal\training_caching\Services\TrainingDrupalApiManager
+   */
+  protected $trainingDrupalApiManager;
+
+  /**
+   * Construct.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param string $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   Drupal\Core\Entity\EntityTypeManagerInterface.
+   * @param Drupal\training_caching\Services\TrainingDrupalApiManager $api_manager
+   *   Drupal\training_caching\Services\TrainingDrupalApiManager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, TrainingDrupalApiManager $api_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+    $this->trainingDrupalApiManager = $api_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('training_caching.manager'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $node = $this->getContextValue('node');
+    $node_list = $this->trainingDrupalApiManager->getTopNNodes(NULL, 3);
+    $content = [
+      '#theme' => 'item_list',
+      '#list_type' => 'ul',
+      '#title' => 'Top 3 content',
+      '#items' => $node_list,
+    ]; 
+    return $content;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheTags() {
+    $node = $this->getContextValue('node');
+    $node_list = $this->trainingDrupalApiManager->getTopNNodes(NULL, 3);
+    $list = [];
+    $cache_tags = $node->getCacheTags();
+    if ($node_list) {
+      foreach ($node_list as $entity_id => $entity_title) {
+        $cache_tags[] = "node:" . $entity_id;
+      }
+    }
+    return Cache::mergeTags(
+      parent::getCacheTags(),
+      $cache_tags
+    );
+  }
+
+}
---
+++ docroot/modules/custom/training_caching/src/Services/TrainingDrupalApiManager.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Drupal\training_caching\Services;
+
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityDisplayRepositoryInterface;
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+
+/**
+ * Service manager for QED training.
+ *
+ * Provides a Space for implementing Generic.
+ * Functionalities for the Platform.
+ */
+class TrainingDrupalApiManager {
+
+  /**
+   * The Config Factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * The entity type bundle info.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeBundleInfoInterface
+   */
+  protected $entityTypeBundleInfo;
+
+  /**
+   * The entity Display Repository service.
+   *
+   * @var \Drupal\Core\Entity\EntityDisplayRepositoryInterface
+   */
+  protected $entityDisplayRepository;
+
+  /**
+   * Logger service.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  protected $loggerFactory;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a VeoliaPlatformManager object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   The Config Factory.
+   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $entityTypeBundleInfo
+   *   The entity type bundle info.
+   * @param \Drupal\Core\Entity\EntityDisplayRepositoryInterface $entityDisplayRepository
+   *   The entity Display Repository service.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $loggerFactory
+   *   The Logger Service.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   Entity type manager.
+   */
+  public function __construct(
+    ConfigFactoryInterface $configFactory,
+    EntityTypeBundleInfoInterface $entityTypeBundleInfo,
+    EntityDisplayRepositoryInterface $entityDisplayRepository,
+    LoggerChannelFactoryInterface $loggerFactory,
+    EntityTypeManagerInterface $entityTypeManager,
+  ) {
+    $this->configFactory = $configFactory;
+    $this->entityTypeBundleInfo = $entityTypeBundleInfo;
+    $this->entityDisplayRepository = $entityDisplayRepository;
+    $this->loggerFactory = $loggerFactory;
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * Helper method to get top N nodes.
+   *
+   * @param string $bundle
+   *   The Entity Bundle.
+   * @param integer $count
+   *   The range for the query.
+   */
+  public function getTopNNodes(string $bundle = NULL, int $count = 3): array {
+    $entities = [];
+    $storage = $this->entityTypeManager->getStorage('node');
+    $query_result = $storage->getQuery();
+    $query_result->accessCheck(FALSE);
+    if ($bundle) {
+      $query_result->condition('bundle', $bundle);
+    }
+    $query_result->sort('changed','DESC');
+    $query_result->range(0, $count);
+    $entities = $query_result->execute();
+    if ($entities) {
+      foreach ($storage->loadMultiple($entities) as $key => $entity) {
+        $list[$entity->id()] = $entity->getTitle();
+      }
+    }
+    return $list;
+  }
+
+  /**
+   * Helper method to invalidate cache of top N nodes.
+   */
+  public function invalidateCacheofTopNNodes(): bool {
+    $node_list = $this->trainingDrupalApiManager->getTopNNodes(NULL, 3);
+    $cache_tags = [];
+    if ($node_list) {
+      foreach ($node_list as $entity_id => $entity_title) {
+        $cache_tags[] = "node:" . $entity_id;
+      }
+      return Cache::invalidateTags($cache_tags) ? TRUE : FALSE;
+    }
+    return FALSE;
+  }
+
+}
